5f2aef9aa7c69d7b770a3e1dbd6fc215641c3372
olilari <larissa.paula.oliveira03@escola.pr.gov.br>
2025-09-16 12:26:17 -0300

    Add files via upload

---
+++ Projeto-Larissa-1D/page.php
@@ -0,0 +1,27 @@
+<?php
+include "conexao.php"; // inclui o arquivo de conexão
+
+$result = mysqli_query($conexao, "SELECT * FROM pedidos"); // exemplo de consulta
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <form>
+<style>
+   
+        <input type="text">
+        <input type="password">
+        <input type="email">
+
+        <button type="submit">
+
+</body>
+</html>
